7edf08da94e9498c98df6f4494ddcda4b70d5291
bjl14710 <bjl14710@gmail.com>
2021-10-31 18:32:05 -0700

    got the coordinates in the csv file

--- readFromInput.py
+++ readFromInput.py
@@ -1,6 +1,8 @@
 import csv
 
-with open('test.csv') as csv_file:
+xCoord = []
+yCoord = []
+with open('testData - Sheet1.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
@@ -8,6 +10,10 @@ with open('test.csv') as csv_file:
             print(f'Coloumn names are {", ".join(row)}')
             line_count += 1
         else:
+            xCoord.append(row[1])
+            yCoord.append(row[2])
             line_count += 1
     print(f'Processed {line_count} lines.')
     
+print(str(xCoord))
+print(str(yCoord))
