b3506e1ae9114c929905e6379e994df7e5958714
bjl14710 <bjl14710@gmail.com>
2021-11-13 20:37:30 -0700

    got things on my 3d plot

--- readFromInput.py
+++ readFromInput.py
@@ -5,11 +5,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 # from scipy.spatial import Delaunay, delaunay_plot_2d
 from matplotlib.tri import triangulation
-from scipy.spatial import ConvexHull, Delaunay
+from scipy.spatial import ConvexHull, Delaunay, delaunay_plot_2d
 from mpl_toolkits import mplot3d
 
 
-
+class Coord:
+    def __init__(self,point,size):
+        self.point = point
+        self.size = size
 
 xCoord = []
 yCoord = []
@@ -39,6 +42,10 @@ with open('data/hygdata_v3.csv') as csv_file:
             xCoord.append(row[17])
             yCoord.append(row[18])
             zCoord.append(row[19])
+            # xCoord[i] = row[17]
+            # yCoord[i] = row[18]
+            # zCoord[i] = row[19]
+            
             ListaDelDistancia.append(row[9])
             line_count += 1
         else:
@@ -63,38 +70,55 @@ def constructArray(x,y,z):
 
 
 
-def plot_tri_simple(ax, points, tri):
-    for tr in tri.simplices:
-        pts = points[tr,: ]
-    ax.plot3D(pts[[0, 1], 0], pts[[0, 1], 1], pts[[0, 1], 2], color = 'g', lw = '0.1')
-    ax.plot3D(pts[[0, 2], 0], pts[[0, 2], 1], pts[[0, 2], 2], color = 'g', lw = '0.1')
-    ax.plot3D(pts[[0, 3], 0], pts[[0, 3], 1], pts[[0, 3], 2], color = 'g', lw = '0.1')
-    ax.plot3D(pts[[1, 2], 0], pts[[1, 2], 1], pts[[1, 2], 2], color = 'g', lw = '0.1')
-    ax.plot3D(pts[[1, 3], 0], pts[[1, 3], 1], pts[[1, 3], 2], color = 'g', lw = '0.1')
-    ax.plot3D(pts[[2, 3], 0], pts[[2, 3], 1], pts[[2, 3], 2], color = 'g', lw = '0.1')
+# def plot_tri_simple(ax, points, tri):
+#     for tr in tri.simplices:
+#         pts = points[tr,: ]
+#     ax.plot3D(pts[[0, 1], 0], pts[[0, 1], 1], pts[[0, 1], 2], color = 'g', lw = '0.1')
+#     ax.plot3D(pts[[0, 2], 0], pts[[0, 2], 1], pts[[0, 2], 2], color = 'g', lw = '0.1')
+#     ax.plot3D(pts[[0, 3], 0], pts[[0, 3], 1], pts[[0, 3], 2], color = 'g', lw = '0.1')
+#     ax.plot3D(pts[[1, 2], 0], pts[[1, 2], 1], pts[[1, 2], 2], color = 'g', lw = '0.1')
+#     ax.plot3D(pts[[1, 3], 0], pts[[1, 3], 1], pts[[1, 3], 2], color = 'g', lw = '0.1')
+#     ax.plot3D(pts[[2, 3], 0], pts[[2, 3], 1], pts[[2, 3], 2], color = 'g', lw = '0.1')
 
-    ax.scatter(points[: , 0], points[: , 1], points[: , 2], color = 'b')
+#     ax.scatter(points[: , 0], points[: , 1], points[: , 2], color = 'b')
 
-### calling with random points
-np.random.seed(0)
-x = 2.0 * np.random.rand(20) - 1.0
-y = 2.0 * np.random.rand(20) - 1.0
-z = 2.0 * np.random.rand(20) - 1.0
-points = np.vstack([x, y, z]).T
-tri = Delaunay(points)
+# ### calling with random points
+# np.random.seed(0)
+# # x = 2.0 * np.random.rand(20) - 1.0
+# # y = 2.0 * np.random.rand(20) - 1.0
+# # z = 2.0 * np.random.rand(20) - 1.0
+# x = 2.0 * xCoord - 1
+# y = 2.0 * yCoord - 1
+# z = 2.0 * zCoord - 1
 
-fig = plt.figure()
-ax = plt.axes(projection = '3d')
-plot_tri_simple(ax, points, tri)
+# points = np.vstack([x, y, z]).T
+# tri = Delaunay(points)
 
-
-
-
-### plotting 
-
+# fig = plt.figure()
+# ax = plt.axes(projection = '3d')
+# plot_tri_simple(ax, points, tri)
 
 
 
+
+### second attempt
+
+
+
+
+# fig = plt.figure()
+
+# fig = plt.figure(figsize = (10,10))
+
+# ax = fig.add_subplot(projection='3d')
+# ax = plt.axes(projection='3d')
+
+# ax.plot3D(xCoord,yCoord,zCoord,'gray')
+
+# ax.plot_trisurf(xCoord, yCoord, zCoord, color='white', edgecolors='grey', alpha=0.5)
+# ax.scatter3D(xCoord, yCoord, zCoord, color = 'b')
+
+# ax.plot(xCoord, yCoord, zCoord, color='r')
 ### Constructing the graph
 arr = constructArray(xCoord,yCoord,zCoord)
 # print('array is ' + str(arr))
@@ -105,11 +129,66 @@ arr = constructArray(xCoord,yCoord,zCoord)
 # print(str(indices))
 # vertices = arr[indices]
 
-'''
-points = constructPoints(xCoord,yCoord)
-# print(str(points))
-tri = Delaunay(points)
-_ = delaunay_plot_2d(tri)
-plt.scatter(xCoord,yCoord)
-'''
+
+# points = constructPoints(xCoord,yCoord)
+# # print(str(points))
+# tri = Delaunay(points)
+# _ = delaunay_plot_2d(tri)
+# plt.scatter(xCoord,yCoord)
+
+# z = np.random.randint(100, size =(50))
+# x = np.random.randint(80, size =(50))
+# y = np.random.randint(60, size =(50))
+ 
+# x = [] * len(xCoord)
+# y = [] * len(yCoord)
+# z = [] * len(zCoord)
+
+
+
+# fig = plt.figure(figsize = (10, 7))
+# ax = plt.axes(projection ="3d")
+
+
+# ax.scatter3D(xCoord,yCoord,zCoord, color = "green")
+# ax.scatter3D(xCoord,yCoord,zCoord)
+
+# plt.show()
+
+
+# fig = plt.figure(figsize=(4,4))
+
+# ax = fig.add_subplot(111, projection='3d')
+# for i in range(len(xCoord)):
+#     ax.scatter(xCoord[i],yCoord[i],zCoord[i])
+
+# plt.show()
+
+
+fig = plt.figure()
+ax = plt.axes(projection="3d")
+
+z_line = np.linspace(0, 15, 1000)
+x_line = np.cos(z_line)
+y_line = np.sin(z_line)
+ax.plot3D(x_line, y_line, z_line, 'gray')
+
+z_points = 15 * np.random.random(len(zCoord))
+# x_points = np.cos(z_points) + 0.1 * np.random.randn(100)
+# y_points = np.sin(z_points) + 0.1 * np.random.randn(100)
+
+xCoord = np.array(xCoord,dtype = float)
+
+yCoord = np.array(yCoord,dtype = float)
+
+zCoord = np.array(zCoord,dtype = float)
+
+ax.scatter3D(xCoord, yCoord, zCoord, c=z_points, cmap='hsv');
+
 plt.show()
+
+
+
+
+
+
