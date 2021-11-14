e0719fe93c9fae6b452b24fbb80343d49e15c1e3
bjl14710 <bjl14710@gmail.com>
2021-11-13 18:58:47 -0700

    initial triangulation

--- readFromInput.py
+++ readFromInput.py
@@ -3,7 +3,10 @@ import os
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.spatial import Delaunay, delaunay_plot_2d
+# from scipy.spatial import Delaunay, delaunay_plot_2d
+from matplotlib.tri import triangulation
+from scipy.spatial import ConvexHull, Delaunay
+from mpl_toolkits import mplot3d
 
 
 
@@ -59,11 +62,46 @@ def constructArray(x,y,z):
     return arr
 
 
+
+def plot_tri_simple(ax, points, tri):
+    for tr in tri.simplices:
+        pts = points[tr,: ]
+    ax.plot3D(pts[[0, 1], 0], pts[[0, 1], 1], pts[[0, 1], 2], color = 'g', lw = '0.1')
+    ax.plot3D(pts[[0, 2], 0], pts[[0, 2], 1], pts[[0, 2], 2], color = 'g', lw = '0.1')
+    ax.plot3D(pts[[0, 3], 0], pts[[0, 3], 1], pts[[0, 3], 2], color = 'g', lw = '0.1')
+    ax.plot3D(pts[[1, 2], 0], pts[[1, 2], 1], pts[[1, 2], 2], color = 'g', lw = '0.1')
+    ax.plot3D(pts[[1, 3], 0], pts[[1, 3], 1], pts[[1, 3], 2], color = 'g', lw = '0.1')
+    ax.plot3D(pts[[2, 3], 0], pts[[2, 3], 1], pts[[2, 3], 2], color = 'g', lw = '0.1')
+
+    ax.scatter(points[: , 0], points[: , 1], points[: , 2], color = 'b')
+
+### calling with random points
+np.random.seed(0)
+x = 2.0 * np.random.rand(20) - 1.0
+y = 2.0 * np.random.rand(20) - 1.0
+z = 2.0 * np.random.rand(20) - 1.0
+points = np.vstack([x, y, z]).T
+tri = Delaunay(points)
+
+fig = plt.figure()
+ax = plt.axes(projection = '3d')
+plot_tri_simple(ax, points, tri)
+
+
+
+
+### plotting 
+
+
+
+
+### Constructing the graph
 arr = constructArray(xCoord,yCoord,zCoord)
 # print('array is ' + str(arr))
-tri = Delaunay(arr) # points: np.array() of 3d points 
-indices = tri.simplices
-print('3d triangulation is ' + str(tri))
+# tri = Delaunay(arr) # points: np.array() of 3d points 
+# tri = triangulation(xCoord,yCoord,zCoord) # points: np.array() of 3d points 
+# indices = tri.simplices
+# print('3d triangulation is ' + str(tri))
 # print(str(indices))
 # vertices = arr[indices]
 
